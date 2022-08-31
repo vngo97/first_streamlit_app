4e6dff9538e3117a3767122c82faf990a065c73a
vngo97 <112349489+vngo97@users.noreply.github.com>
2022-08-31 16:08:36 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,6 @@ try:
 except URLError as e:
     streamlit.error()
 
-###
 streamlit.header("The fruit load list contains:")
 #Snowflake-related functions
 def get_fruit_load_list():
@@ -56,19 +55,24 @@ if streamlit.button('Get fruit list'):
     my_data_rows = get_fruit_load_list()
     my_cnx.close()
     streamlit.dataframe(my_data_rows)
-####
+
+
+#Allow the end user to add a fruit to the list
+def insert_row_snowflake(new_fruit):
+    with my_cnx.cursor() as my_cur:
+         my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+         return "Thanks for adding" + new_fruit
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+if streamlit.button('Add a Fruit to the List'):
+   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+   back_from_function = insert_row_snowflake(add_my_fruit)
+   streamlit.text(back_from_function)
+
 
 #Don't run anything past here while we troubleshoot
 streanlit.stop()
 #import snowflake.connector
-
-#Allow the end user to add a fruit to the list
-add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
-streamlit.write('Thanks for adding', add_my_fruit)
-
-
 #This will not work correctly, but just fo with it for now
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 
 #Don't run anything past here while we troubleshoot
 #streanlit.stop()
